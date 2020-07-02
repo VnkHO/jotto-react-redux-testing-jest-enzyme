5f63c91945e4cb0c352a00dddb5750374398098f
Vnk <vnk@MacBook-Pro-de-Vnk.local>
2020-07-02 21:30:16 +0200

    props types and default props

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -3,10 +3,12 @@ import React from 'react'
 import Enzyme, {shallow, ShallowWrapper} from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 
-import {findByTestAttr} from '../test/testUtils'
+import {findByTestAttr, checkProps, checkPropTypes} from '../test/testUtils'
 import Congrats from './Congrats'
 
 Enzyme.configure({adapter: new EnzymeAdapter()})
+
+const defaultProps = {success: false}
 
 /**
  * Factory function to create a ShallowWrapper for the Congrats component.
@@ -16,11 +18,12 @@ Enzyme.configure({adapter: new EnzymeAdapter()})
  */
 
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />)
+  const setupProps = {...defaultProps, ...props}
+  return shallow(<Congrats {...setupProps} />)
 }
 
 test(`render without error`, () => {
-  const wrapper = setup()
+  const wrapper = setup({success: false})
   const component = findByTestAttr(wrapper, 'component-congrats')
   expect(component.length).toBe(1)
 })
@@ -35,4 +38,9 @@ test(`rebders non-empty congrats message when success prop is true`, () => {
   const wrapper = setup({success: true})
   const message = findByTestAttr(wrapper, 'congrats-message')
   expect(message.text().length).not.toBe(0)
+})
+
+test('does not throw warning with expected props', () => {
+  const expectedProps = {success: false}
+  checkProps(Congrats, expectedProps)
 })
